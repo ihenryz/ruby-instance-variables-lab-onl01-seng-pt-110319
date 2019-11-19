6a50e8c1583e077eed01f0fccfccdfd63bf66b60
Henry Phan <h.phan06@gmail.com>
2019-11-19 22:10:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog 
 
-def name=(dog_name)
-  @this_dogs = dog_name
-end
+  attr_accessor :this_dogs_name
 
-def name 
-  @this_dogs
-end
+# def name=(dog_name)
+#   @this_dogs_name = dog_name
+# end
+
+# def name 
+#   @this_dogs_name
+# end
 
 end
 
