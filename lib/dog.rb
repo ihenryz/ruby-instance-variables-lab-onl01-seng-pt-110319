dd5c0623f036244acc67f5772ac04eb38ec5fae2
Henry Phan <h.phan06@gmail.com>
2019-11-19 22:09:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,14 @@
 class Dog 
 
-  def this_dogs_name=(this_dogs_name)
-    @this_dogs_name = this_dogs_name
-  end
-  
-  def this_dogs_name
-    @this_dogs_name
-  end
-  
-  def initialize(this_dogs_name)
-    @name = this_dogs_name
-  end
+def name=(dog_name)
+  @this_dogs_name = dog_name
+end
+
+def name 
+  @this_dogs_name
+end
 
 end
 
 lassie = Dog.new 
+lassie.name = "Lassie"
