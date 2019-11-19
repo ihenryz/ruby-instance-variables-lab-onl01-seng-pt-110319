31caea044e5ac286e7031b3e57ce1988bc5bde70
Henry Phan <h.phan06@gmail.com>
2019-11-19 22:07:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
-class Dog
+class Dog 
+
+  attr_accessor :this_dogs_name
   
-  attr_accessor :dog_name
-  
-  def initialize(dog_name)
-    @name = dog_name
-  end
-  
-  def name
-    @name
-  end
-  
+
 end
-# lassie.name = "Lassie"
